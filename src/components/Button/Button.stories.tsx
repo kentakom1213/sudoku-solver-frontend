02c1278d6852423cc4dca8bbc88d7dfd447eb6e4
kentakom1213 <kentakom1213@gmail.com>
2023-06-28 02:08:56 +0900

    [add] tailwindの追加

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -5,5 +5,18 @@ export default {
   component: Button,
 }
 
-export const HelloButton = () => <Button>Hello World!</Button>;
-export const ClickButton = () => <Button>Click!</Button>;
+// 数字を表示するボタン
+export const NumberButton = ({ num, ...props }: { num: number, props?: any }) => (
+  <Button {...props} className="h-20 w-20 border-4 border-black rounded-2xl text-5xl">
+    {num}
+  </Button>
+);
+
+// 1のボタン（テスト）
+export const NumberButton1 = () => <NumberButton num={1} />;
+
+// 2のボタン（テスト）
+export const NumberButton2 = () => <NumberButton num={2} />;
+
+// APIを呼び出すボタン
+export const EnterButton = () => <Button>Enter</Button>;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,11 @@
-function Button({ children } : any) {
-  return <button>{children}</button>;
-}
+import { ReactNode } from "react";
+
+const Button = ({ className, children }: { className?: string, children: ReactNode }) => {
+  return (
+    <button className={className}>
+      {children}
+    </button>
+  );
+};
 
 export default Button;
