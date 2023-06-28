dac3fdef5fe4a6c10c3b2ee85d9f610422c267fe
kentakom1213 <kentakom1213@gmail.com>
2023-06-28 21:57:03 +0900

    [fix] サイズの調整

--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -4,7 +4,7 @@ export const Field = ({ data }: { data: FieldData }) => {
   let cells = Array.from(data.field.flat().entries())
               .map(([i, d]: [number, number]) => <Cell key={i} num={d} />);
   return (
-    <div className="h-auto w-auto grid grid-cols-9 border-4 border-black">
+    <div className="h-auto w-auto grid grid-cols-9 rounded-md border-4 border-black">
       {cells}
     </div>
   );
