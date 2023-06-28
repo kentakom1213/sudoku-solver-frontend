b230f9a761c7da839c765fe4a4b59cdaf378d157
kentakom1213 <kentakom1213@gmail.com>
2023-06-28 12:03:25 +0900

    [add] ボタンにモーションを追加

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -6,22 +6,15 @@ export default {
 }
 
 // 数字を表示するボタン
-export const NumberButton = ({ num, ...props }: { num: number, props?: any }) => (
-  <Button {...props} className="h-20 w-20">
+export const NumberButton = ({ num, is_selected }: { num: number, is_selected: boolean }) => (
+  <Button className={`h-20 w-20 ${is_selected ? 'btn-selected' : ''}`}>
     {num}
   </Button>
 );
 
-// 数字のボタン
-export const NumberButton1 = () => <NumberButton num={1} />;
-export const NumberButton2 = () => <NumberButton num={2} />;
-export const NumberButton3 = () => <NumberButton num={3} />;
-export const NumberButton4 = () => <NumberButton num={4} />;
-export const NumberButton5 = () => <NumberButton num={5} />;
-export const NumberButton6 = () => <NumberButton num={6} />;
-export const NumberButton7 = () => <NumberButton num={7} />;
-export const NumberButton8 = () => <NumberButton num={8} />;
-export const NumberButton9 = () => <NumberButton num={9} />;
+// 数字のボタン（テスト用）
+export const NumberButton1_true = () => <NumberButton num={1} is_selected={true} />;
+export const NumberButton1_false = () => <NumberButton num={1} is_selected={false} />;
 
 // APIを呼び出すボタン
 export const EnterButton = (props: any) => (
