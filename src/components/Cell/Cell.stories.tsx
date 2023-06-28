17f767f1f56fe69445c34b0b518d5195df7a77ad
kentakom1213 <kentakom1213@gmail.com>
2023-06-28 17:09:53 +0900

    [fix] ボツ案の削除 / Cellのstory追加

---
+++ src/components/Cell/Cell.stories.tsx
@@ -0,0 +1,8 @@
+import { Cell } from "./Cell";
+
+export default {
+  title: "Cell",
+  component: Cell,
+}
+
+export const Cell1 = () => <Cell num={1} />
--- src/components/Field/Field.tsx
+++ src/components/Field/Field.tsx
@@ -1,70 +1,11 @@
-import { data } from "autoprefixer";
 import { Cell } from "../Cell/Cell";
 import "./field.css";
 
-// fieldの座標をBlockの座標に変換する
-const fieldToBlock = (r: number, c: number) => {
-  let block_r = r / 3;  // 所属するブロックのrow
-  let block_c = c / 3;  // 所属するブロックのcolumn
-  let block_id = 3 * block_r + block_c;  // 所属するブロックの座標
-  let rr = r % 3;  // 所属するブロック内でのrow
-  let cc = c % 3;  // 所属するブロック内でのcolumn
-  let inner_id = 3 * rr + cc;
-  return [block_id, inner_id];
-};
-
-// ブロックを切り出す
-const getBlocks = (array: Field9x9) => {
-  // 配列の初期化
-  let res: Blocks = [
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0]
-  ];
-
-  // コピーしていく
-  for (let i = 0; i < 9; i++) {
-    for (let j = 0; j < 9; j++) {
-      let [r, c] = fieldToBlock(i, j);
-      res[r][c] = array[i][j];
-    }
-  }
-
-  return res;
-};
-
-// ブロックを描画する
-export const Block = ({ block }: { block: Row }) => {
-  let blocks = block.map((d: number) => <Cell num={d} />);
-  return (
-    <span>
-      {blocks}
-    </span>
-  );
-};
-
 export const Field = ({ data }: { data: FieldData }) => {
-  let blocks = getBlocks(data.field)
-                .map((block) => <Block block={block} />);
-
+  let cells = data.field.flat().map(((d: number) => <Cell num={d} />));
   return (
     <div className="h-96 w-96 grid grid-cols-9">
-      {blocks}
+      {cells}
     </div>
   );
 };
-
-// export const Field = ({ data }: { data: FieldData }) => {
-//   let cells = data.field.flat().map(((d: number) => <Cell num={d} />));
-//   return (
-//     <div className="h-96 w-96 grid grid-cols-9">
-//       {cells}
-//     </div>
-//   );
-// };
